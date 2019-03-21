baa911581b20e8ae06bbde594e3d1a874366d13e
Ashley Radford <aradford7@outlook.com>
2019-03-21 16:56:29 -0700

    Commit 1-Verbal questions

--- hw3.js
+++ hw3.js
@@ -1,1 +1,16 @@
 console.log('JS is runnning and running and running')
+
+//Verbal questions
+//Write answers to the following questions as comments.
+
+//What is the difference between a parameter and an argument?
+    //A parameter is a variable in a method defination
+    //Parameter is variable in declaration of function. When a method is called
+    //the arguments are the data that passes thru the methods' parameters.
+    //Arguments is the actual value of this variable that gets passed to function.
+
+//Within a function, what is the difference between return and console.log?
+    //console.log is mainly for developers to check to see if script is running.
+    //return does not appear in the console and it will end the loop or function
+
+//What are the implications of the ability of a function to return a value?
